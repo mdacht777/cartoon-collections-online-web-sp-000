04f53391aad35042d197300109ce4124e64da031
Martin Achtermann <mdacht777@gmail.com>
2020-06-13 13:47:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,8 @@ def long_planeteer_calls(arr)# code an argument here
   false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  puts cheese_types.union(arr)
 end
