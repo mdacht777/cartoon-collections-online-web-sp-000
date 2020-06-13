81ee94c5f9c9315d75ea952a0b04eee786f8f8a9
Martin Achtermann <mdacht777@gmail.com>
2020-06-13 13:53:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   arr.each_with_index { |item, index|
-   if cheese_types.include(item)
+   if cheese_types.include?(item)
      return item
    end
   }
