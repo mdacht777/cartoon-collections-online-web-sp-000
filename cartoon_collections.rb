4fc30c1fc94e751b7a63e9fc3d9c69a0f0d82b53
Martin Achtermann <mdacht777@gmail.com>
2020-06-13 13:48:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,5 +26,6 @@ end
 def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  puts cheese_types.union(arr)
+  arr1=cheese_types.union(arr)
+  puts arr1
 end
